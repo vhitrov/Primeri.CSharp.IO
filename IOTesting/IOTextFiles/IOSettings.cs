1691cba6776ae33a7fab43f850c0030fae3e005d
Vasil Hitrov <vhitrov@hotmail.com>
2015-11-03 10:18:50 +0200

    Модул 08, Видео 02 - Запис на текстов файл.

---
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IOTextFiles
+{
+	public class IOSettings
+	{
+		private STable _stable;
+
+		public IOSettings (STable stable)
+		{
+			_stable = stable;
+
+		}
+
+		public bool save ()
+		{
+			try
+			{
+				string _temp = "";
+
+				_temp = String.Join ("\r\n", _stable.stable);
+
+				//Запис на тектов файл
+				System.IO.File.WriteAllText ("D:\\aula\\test.txt", _temp);
+
+				return true;
+			}catch{
+			}
+			return false;
+		}
+	}
+}
+
---
+++ IOTesting/IOTextFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IOTextFiles
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			STable _STable = new STable ();
+			IOSettings _io = new IOSettings (_STable);
+
+			Console.WriteLine (_STable.stable [0]);
+			Console.WriteLine (_STable.stable [1]);
+			Console.WriteLine (_STable.stable [2]);
+			Console.WriteLine (_STable.stable [3]);
+
+			if (_io.save ()) {
+				Console.WriteLine ("Таблицата е запаметена успешно.");
+			} else {
+				Console.WriteLine ("Таблицата НЕ е запаметена успешно.");
+			}
+		}
+	}
+}
