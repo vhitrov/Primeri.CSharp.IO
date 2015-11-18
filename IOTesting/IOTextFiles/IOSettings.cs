287046440e36fe90895ea0df94d701ebfce26890
Vasil Hitrov <vhitrov@hotmail.com>
2015-11-18 19:19:58 +0200

    Модул 08, Видео 06 - Създаване на директория

--- IOTesting/IOTextFiles/IOSettings.cs
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -14,14 +14,15 @@ namespace IOTextFiles
 
 		public string getPath ()
 		{
-			string _path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+			string _path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "Settings");
+			_path = System.IO.Path.Combine (_path, "Settings.txt");
 
 			//Други видове директории
-			string _user=Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-			Console.WriteLine (_user);
+			//string _user=Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+			//Console.WriteLine (_user);
 
-			string _desktop = Environment.GetFolderPath (Environment.SpecialFolder.Desktop);
-			Console.WriteLine (_desktop);
+			//string _desktop = Environment.GetFolderPath (Environment.SpecialFolder.Desktop);
+			//Console.WriteLine (_desktop);
 
 			return _path;
 		}
@@ -34,7 +35,7 @@ namespace IOTextFiles
 
 				_temp = String.Join ("\r\n", _stable.stable);
 
-				//Запис на тектов файл
+				//Запис на текстов файл
 				System.IO.File.WriteAllText (getPath(), _temp);
 
 				return true;
@@ -47,7 +48,10 @@ namespace IOTextFiles
 		{
 			try
 			{
+				iniSettings();
+
 				string _temp="", _filePath=getPath();
+
 				if(System.IO.File.Exists(_filePath)) //Проверка дали пътя е валиден
 				{
 					System.IO.File.ReadAllText(_filePath);
@@ -59,9 +63,8 @@ namespace IOTextFiles
 					_stable.stable[i]=_table[i];
 				}
 				}else{
-					Console.WriteLine("Не е намерен такъв път.");
+					
 					return false;
-
 				}
 				return true;
 
@@ -69,6 +72,30 @@ namespace IOTextFiles
 
 				return false;
 				}
+		}
+
+		private void iniSettings ()
+		{
+			try
+			{
+				bool _fileExist=System.IO.File.Exists(getPath());
+
+				if(!_fileExist)
+				{
+					string _directory=System.IO.Path.GetDirectoryName(getPath());
+
+					//Застраховаме се, че директорията съществува
+					if(!System.IO.Directory.Exists(_directory))
+					{
+						System.IO.Directory.CreateDirectory(_directory);
+					}
+
+					//Запаметяване съдържанието на файла
+					save();
+
+				}
+			}catch{
+			}
 		}
 		}
 
