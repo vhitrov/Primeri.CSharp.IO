130a31dcb7a22823e44a4b81b114a55757fcc0cd
Vasil Hitrov <vhitrov@hotmail.com>
2015-11-22 09:57:01 +0200

    Модул 08, Видео 08 - Въведение в XML и създаване на файл

---
+++ IOTesting/IOXMLFiles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace IOXMLFiles
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string value = "simple value", property = "simple property";
+
+			string path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "test.xml");
+			using (XmlWriter writer = XmlWriter.Create (path)) {
+				writer.WriteStartDocument ();
+				writer.WriteStartElement ("Settings");
+
+				//Съдържание на файла
+				writer.WriteStartElement ("row");
+
+				writer.WriteAttributeString ("property", property); //<row property="..."> ...</row>
+				writer.WriteString (value); //<raw> value </row>
+
+				writer.WriteEndElement ();
+
+
+				writer.WriteEndElement ();
+
+				writer.WriteEndDocument ();
+			}
+
+			XDocument document = XDocument.Load (path);
+			document.Save (path);
+
+			System.Diagnostics.Process.Start(path);
+
+
+		}
+	}
+}
